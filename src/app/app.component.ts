da7c2229e8b378bea714299a1946f1c30cf1a466
ravanasura111 <rameshkumariosapp@gmail.com>
2021-06-21 11:28:36 +0530

    Intial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'bwm-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  constructor() {}
+  
+
+  ngOnInit() {
+  }
+
+}
